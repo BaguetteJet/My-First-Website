61c573311e9a87af430e0acd537248a96cfff47a
BaguetteJet <117129969+BaguetteJet@users.noreply.github.com>
2024-09-07 01:19:07 +0100

    fixes & new menu

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
+// logo parallax
 const parallax = document.getElementById("parallax");
 var distanceToTop = window.scrollY + parallax.getBoundingClientRect().top
 window.addEventListener("scroll", function()
 {   
     let distanceToTop = parallax.getBoundingClientRect().top
-    if (window.scrollY >= distanceToTop) {
-        let offset = window.scrollY;
-        parallax.style.backgroundPositionX = window.innerWidth + 2000 + -4 * offset + "px";
+    let distnaceToStart = parallax.offsetTop
+    if (distanceToTop <= -100) {
+        let offset = window.scrollY - distnaceToStart;
+        parallax.style.backgroundPositionX = window.innerWidth + 500 + -4 * offset + "px";
         parallax.style.backgroundPositionY = offset * 0.9 - distanceToTop + "px";
     }
     else {
         parallax.style.backgroundPositionX = window.innerWidth + 2000 + "px";
     }
 })
-
+// theme
 function themeMode() {
     var element = document.body;
     let content = document.getElementById("themeButton");
     if (content.innerText == "Light") content.innerText = "Dark";
-    else content.innerText = "Light"
+    else content.innerText = "Light";
     element.classList.toggle("light-mode");
 }
+// menu
+const menuButton = document.getElementById('menu-button');
+const menu = document.getElementById('menu');
+    // show/hide menu when button clicked
+menuButton.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+})
+    // hide menu when click event occurs outside menu
+document.addEventListener('click', (event) => {
+  if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
+    menu.classList.add('hidden');
+  }
+})
+
